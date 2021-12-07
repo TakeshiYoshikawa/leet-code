457903d1893f4bcc74e86b6c453856f21932e484
Arthur Takeshi <takeshiy096@gmail.com>
2021-12-07 03:53:06 -0300

    Create coin_change.cpp

---
+++ coin_change.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> change(amount + 1, amount + 1);
+        
+        change[0] = 0;
+        for(int coin : coins){
+            for(int j = coin; j <= amount; j++){
+                change[j] = min(change[j - coin] + 1, change[j]);
+            }
+        }
+        
+        return change[amount] != amount + 1 ? change[amount] : -1;
+    }
+};
